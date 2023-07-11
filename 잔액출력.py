a3e403b6f7cf8bf35b42f1c7bbeba24760091972
yunji lee <lyj001013@gmail.com>
2023-07-11 23:20:12 +0900

    feat : accountList 정의

--- 잔액출력.py
+++ 잔액출력.py
@@ -1,1 +1,14 @@
 import pandas as pd
+def accountList(account_list):
+    print("--------------")
+    print("계좌목록")
+    print("--------------")
+
+    if len(account_list) == 0:
+        print("등록된 계좌가 없습니다.")
+    else:
+        for account in account_list:
+            print(f"계좌번호: {account['ano']}")
+            print(f"계좌주: {account['owner']}")
+            print(f"잔액: {account['balance']}")
+            print("--------------------")
